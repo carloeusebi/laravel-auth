b8a32226d96580a5cdb03c08ab4ff5a2d323dbbc
carloeusebi <carloeusebi@gmail.com>
2023-09-07 05:59:46 +0200

    Updated seeder to download images

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,19 +5,24 @@ namespace Database\Seeders;
 use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
+        $DIR = 'public/project_images';
+
+        Storage::makeDirectory($DIR);
         // creates 10 records
         for ($i = 0; $i < 10; $i++) {
             Project::create([
                 'name' => "Project $i",
-                'thumbnail' => 'https://picsum.photos/600/600',
+                'thumbnail' => Storage::putFile($DIR, $faker->image(storage_path("app/$DIR"), 600, 600)),
                 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit voluptate veritatis illum velit modi nam tempore fuga, aliquam magnam id. Magni consequuntur eligendi provident quod blanditiis labore eaque et natus.',
             ]);
         }
